2af17ee1252d92e2fb14f32a9379daab3ca8d834
Julien MULLE <j.mulle@groupeonepoint.com>
2022-09-26 11:33:50 +0200

    ajout de devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { rootReducer, metaReducers, ROOT_FEATURE_KEY } from './state/00-reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,11 @@ import { rootReducer, metaReducers, ROOT_FEATURE_KEY } from './state/00-reducer'
       [ROOT_FEATURE_KEY]: rootReducer
     }, {
       metaReducers: metaReducers
-    })
+    }),
+    StoreDevtoolsModule.instrument({
+      name: 'decouverte de ngrx',
+      maxAge: 25, 
+      logOnly: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
